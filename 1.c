7e74ae5fb204938f23010e267df39fc40e3eef49
wqjob <95350607+wqjob@users.noreply.github.com>
2021-12-17 13:12:15 +0800

    Update 1.c

--- 1.c
+++ 1.c
@@ -2,10 +2,8 @@
 int main(int argc, const char *argv[])
 {
 	printf("hello world!\n");
-<<<<<<< HEAD
 	printf("nihao\n");
-=======
 	printf("hello github\n");
->>>>>>> 3dea8999d21b0f8610f0164daaf2af7e3d3f25fc
+	ptintf("nnnnn");
 	return 0;
 }
